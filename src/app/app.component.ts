1f9bc5606c1f6b9ed3095e0b653c50c1951ce441
Damien <damiensandoval15@gmail.com>
2021-02-27 11:06:08 -0800

    added budget calculating function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fiftythirtytwenty';
+  fifty = "50";
+  thirty = "30";
+  twenty = "20";
+  messageDisplay = 'none';
+  numberRequired = '';
+
+  calcBudget(budget:any){
+    this.messageDisplay = 'flex';
+    this.numberRequired = '';
+    this.fifty = (parseInt(budget.target.value) * 0.50).toFixed(2).toString();
+    this.thirty = (parseInt(budget.target.value) * 0.30).toFixed(2).toString();
+    this.twenty = (parseInt(budget.target.value) * 0.20).toFixed(2).toString();
+    if(isNaN(budget.target.value) || budget.target.value == ''){
+      this.numberRequired = 'please enter a valid number'
+      this.messageDisplay = 'none'
+      this.fifty = "50";
+      this.thirty = "30";
+      this.twenty = "20";
+    }
+
+  }
+
 }
